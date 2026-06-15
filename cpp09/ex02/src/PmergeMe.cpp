0e6a65b2e403f59b32b4845fd6a611ce583c27e9
Thomas Black-Roff <thblack-@c1r6p17.hive.fi>
2026-06-15 21:08:06 +0300

    feat: cpp09/ex02 speed of algorithm now measured

--- cpp09/ex02/src/PmergeMe.cpp
+++ cpp09/ex02/src/PmergeMe.cpp
@@ -48,6 +48,50 @@ bool	PmergeMe<T>::isSort( T& data ) {
 	return true;
 };
 
+// Forward declarations of fordJohnsonAlgo() dependencies
+template <typename T>
+static void		sortPairRange( T& data, size_t position, size_t pairSize );
+static size_t	maxElementSize( size_t dataSize );
+template <typename T>
+static void		buildChains( T& main, T& pend, size_t elementSize );
+template <typename T>
+static void		mergeInsert( T& main, T& pend, size_t elementSize );
+template <typename T>
+static void		resolveInsertion( T& main, T& pend, size_t i, size_t treeSize, size_t elementSize );
+static size_t	roundToScale( size_t value, size_t scale );
+static size_t	fetchTreeSize( size_t jN, size_t jC, size_t jP, size_t pS, size_t eS );
+static void		nextJacobsthal( size_t& previousValue, size_t& currentValue, size_t& currentN);
+
+// Main algorithm
+// 1. calls sortPairRange() to do the initial sort of pairs up to max size of
+//    totalSize / pairSize > 0
+// 2. goes into main merge loop starting with largest pairSize decreasing to smallest
+// 3. builds the main and pend chains as arrays of iterator pointers
+// 4. calls mergeInsert()
+// 5. returns sorted data
+template <typename T>
+T	PmergeMe<T>::fordJohnsonAlgo( const T& data ) {
+	T		main(data);
+	size_t	pairSize = 2;
+	while (main.size() / pairSize > 0) {
+		for (size_t i = 0; main.size() - i >= pairSize && i < main.size(); i += pairSize)
+			sortPairRange(main, i, pairSize);
+		pairSize *= 2;
+	}
+	if (main.size() < 3 || isSort(main))
+		return main;
+	T	pend;
+	size_t	elementSize = maxElementSize(main.size());
+	while (elementSize > 0) {
+		buildChains(main, pend, elementSize);
+		mergeInsert(main, pend, elementSize);
+		if (isSort(main))
+			return main;
+		elementSize /= 2;
+	}
+	return main;
+};
+
 // Sorting of pairs done simply with std::swap_range()
 template <typename T>
 static void	sortPairRange( T& data, size_t position, size_t pairSize ) {
@@ -60,6 +104,17 @@ static void	sortPairRange( T& data, size_t position, size_t pairSize ) {
 	auto	pair2first = std::next(data.begin(), position + elementSize);
 	std::swap_ranges(pair1first, pair2first, pair2first); 
 };
+
+// Find the max element size for merge insertion
+// For this we need a minimum 3 of a given element size, representing b1, a1 and
+// the element to be inserted b2
+static size_t	maxElementSize( size_t dataSize ) {
+	size_t	thirdSize = dataSize / 3;
+	size_t	max = 2;
+	while (max <= thirdSize)
+		max *= 2;
+	return max / 2;
+}
 
 // Builds arrays of iterators for main and pend
 // Unsure about where to place b1, into main or pend
@@ -78,57 +133,6 @@ static void	buildChains( T& main, T& pend, size_t elementSize ) {
 			i += elementSize;
 		pendToggle = !pendToggle;
 	}
-};
-
-static size_t	roundToScale( size_t value, size_t scale ) {
-	return (value / scale) * scale;
-}
-
-// Finds position in main for position in pend of elementSize
-// 1. lower and upper limits are gradually narrowed until position found
-// 2. insert() and erase() called on data to move range of data
-// 3. return the updated sorted count
-template <typename T>
-static void	resolveInsertion( T& main, T& pend, size_t i, size_t treeSize, size_t elementSize ) {
-	auto	lower = std::next(main.begin(), elementSize - 1);
-	auto	upper = std::next(main.begin(), (treeSize * elementSize) - 1);
-	auto	pendBegin = std::next(pend.begin(), i * elementSize);
-	auto	pendCompValue = std::next(pendBegin, elementSize - 1);
-	auto	pendEnd = std::next(pendBegin, elementSize);
-	while (lower != upper) {
-		auto middle = std::next(lower, roundToScale(std::distance(lower, upper) / 2, elementSize));
-		if (*pendCompValue < *middle) {
-			if (lower == middle)
-				upper = lower;
-			else
-				upper = std::next(middle, -elementSize);
-		}
-		else
-			lower = std::next(middle, elementSize);
-	}
-	if (*pendCompValue < *lower)
-		main.insert(std::next(lower, 1 - elementSize), pendBegin, pendEnd);
-	else
-		main.insert(std::next(lower, 1), pendBegin, pendEnd);
-	pend.erase(pendBegin, pendEnd);
-};
-
-// Function calculates next value of Jacobsthal sequence
-// Jn+1 = 2Jn + (-1)^n
-static void	nextJacobsthal( size_t& previousValue, size_t& currentValue, size_t& currentN) {
-	size_t	temp = currentValue;
-	currentValue = 2 * currentValue + ((currentN % 2) ? -1 : 1);
-	previousValue = temp;
-	++currentN;
-};
-
-static size_t	fetchTreeSize( size_t jN, size_t jC, size_t jP, size_t pS, size_t eS ) {
-	size_t	maxTreeSize = pow(2, jN - 1) - 1;
-	size_t	treeSize = maxTreeSize - jC + jP
-		+ (pS / eS);
-	if (treeSize > maxTreeSize)
-		treeSize = maxTreeSize;
-	return treeSize;
 };
 
 // Merge insertion loop
@@ -164,43 +168,63 @@ static void	mergeInsert( T& main, T& pend, size_t elementSize ) {
 	}
 };
 
-static size_t	maxElementSize( size_t dataSize ) {
-	size_t	thirdSize = dataSize / 3;
-	size_t	max = 2;
-	while (max <= thirdSize)
-		max *= 2;
-	return max / 2;
-}
-
-// Main algorithm
-// 1. calls sortPairRange() to do the initial sort of pairs up to max size of
-//    totalSize / pairSize > 0
-// 2. goes into main merge loop starting with largest pairSize decreasing to smallest
-// 3. builds the main and pend chains as arrays of iterator pointers
-// 4. calls mergeInsert()
-// 5. returns sorted data
-template <typename T>
-T	PmergeMe<T>::fordJohnsonAlgo( const T& data ) {
-	T		main(data);
-	size_t	pairSize = 2;
-	while (main.size() / pairSize > 0) {
-		for (size_t i = 0; main.size() - i >= pairSize && i < main.size(); i += pairSize)
-			sortPairRange(main, i, pairSize);
-		pairSize *= 2;
-	}
-	if (main.size() < 3 || isSort(main))
-		return main;
-	T	pend;
-	size_t	elementSize = maxElementSize(main.size());
-	while (elementSize > 0) {
-		buildChains(main, pend, elementSize);
-		mergeInsert(main, pend, elementSize);
-		if (isSort(main))
-			return main;
-		elementSize /= 2;
-	}
-	return main;
+// Algorithm for finding the comparison tree size
+// This could be further optimised by ommitting one element from the tree if the
+// previous insertion was placed to the far right, i.e. a-max in the main
+// But this optimisation was considered out of scope for this implementation
+static size_t	fetchTreeSize( size_t jN, size_t jC, size_t jP, size_t pS, size_t eS ) {
+	size_t	maxTreeSize = pow(2, jN - 1) - 1;
+	size_t	treeSize = maxTreeSize - jC + jP
+		+ (pS / eS);
+	if (treeSize > maxTreeSize)
+		treeSize = maxTreeSize;
+	return treeSize;
 };
+
+// Function calculates next value of Jacobsthal sequence
+// Jn+1 = 2Jn + (-1)^n
+static void	nextJacobsthal( size_t& previousValue, size_t& currentValue, size_t& currentN) {
+	size_t	temp = currentValue;
+	currentValue = 2 * currentValue + ((currentN % 2) ? -1 : 1);
+	previousValue = temp;
+	++currentN;
+};
+
+// Finds position in main for position in pend of elementSize
+// 1. lower and upper limits are gradually narrowed until position found
+// 2. insert() and erase() called on data to move range of data
+// 3. return the updated sorted count
+template <typename T>
+static void	resolveInsertion( T& main, T& pend, size_t i, size_t treeSize, size_t elementSize ) {
+	auto	lower = std::next(main.begin(), elementSize - 1);
+	auto	upper = std::next(main.begin(), (treeSize * elementSize) - 1);
+	auto	pendBegin = std::next(pend.begin(), i * elementSize);
+	auto	pendCompValue = std::next(pendBegin, elementSize - 1);
+	auto	pendEnd = std::next(pendBegin, elementSize);
+	while (lower != upper) {
+		auto middle = std::next(lower, roundToScale(std::distance(lower, upper) / 2, elementSize));
+		if (*pendCompValue < *middle) {
+			if (lower == middle)
+				upper = lower;
+			else
+				upper = std::next(middle, -elementSize);
+		}
+		else
+			lower = std::next(middle, elementSize);
+	}
+	if (*pendCompValue < *lower)
+		main.insert(std::next(lower, 1 - elementSize), pendBegin, pendEnd);
+	else
+		main.insert(std::next(lower, 1), pendBegin, pendEnd);
+	pend.erase(pendBegin, pendEnd);
+};
+
+// When moving the upper and lower bounds of the search area of the comparison
+// tree we need to increment and decrement by multiples of the element size
+// thus this algorithm comes in handy
+static size_t	roundToScale( size_t value, size_t scale ) {
+	return (value / scale) * scale;
+}
 
 std::ostream&	operator<<( std::ostream& out, const vector& in ) {
 	std::string	str;
--- cpp09/ex02/src/main.cpp
+++ cpp09/ex02/src/main.cpp
@@ -12,8 +12,8 @@
 
 #include "PmergeMe.hpp"
 #include "PmergeMe.cpp"
-#include <cstdlib>
 #include <iostream> // for std::cout
+#include <chrono> // for time measurements
 
 int	main(int argv, char **argc) {
 	if (argv < 2) {
@@ -25,22 +25,32 @@ int	main(int argv, char **argc) {
 	list	lstInput, lstOutput;
 	try {
 		vecInput = PmergeMe<vector>::loadData(argc + 1);
-		if (!PmergeMe<vector>::isSort(vecInput))
-			vecOutput = PmergeMe<vector>::fordJohnsonAlgo(vecInput);
-		else
-			vecOutput = vecInput;
 		std::cout << "VECTOR\n";
 		std::cout << "Before: " << vecInput << "\n";
+		auto	vecStart = std::chrono::steady_clock::now();
+		vecOutput = PmergeMe<vector>::fordJohnsonAlgo(vecInput);
+		auto	vecEnd = std::chrono::steady_clock::now();
 		std::cout << "After: " << vecOutput << "\n";
-		std::cout << "Result is" << (PmergeMe<vector>::isSort(vecOutput) ? " " : " not ") << "sorted!\n";
+		std::cout << "Vector is" << (PmergeMe<vector>::isSort(vecOutput) ? " " : " not ") << "sorted!\n";
+
+
 		lstInput = PmergeMe<list>::loadData(argc + 1);
-		lstOutput = PmergeMe<list>::fordJohnsonAlgo(lstInput);
 		std::cout << "LIST\n";
 		std::cout << "Before: " << lstInput << "\n";
+		auto	lstStart = std::chrono::steady_clock::now();
+		lstOutput = PmergeMe<list>::fordJohnsonAlgo(lstInput);
+		auto	lstEnd = std::chrono::steady_clock::now();
 		std::cout << "After: " << lstOutput << "\n";
-		std::cout << "Result is" << (PmergeMe<list>::isSort(lstOutput) ? " " : " not ") << "sorted!\n";
-		// vecInput = PmergeMe::loadVectorData(argc + 1);
-		// lstInput = PmergeMe::loadListData(argc + 1);
+		std::cout << "List is" << (PmergeMe<list>::isSort(lstOutput) ? " " : " not ") << "sorted!\n";
+
+
+		std::chrono::duration<double>	vecElapsed = vecEnd - vecStart;
+		std::chrono::duration<double>	lstElapsed = lstEnd - lstStart;
+		std::cout << "RESULTS\n";
+		std::cout << "fordJohnsonAlgo() took " << vecElapsed.count()
+			<< " seconds to sort a vector! :)\n";
+		std::cout << "fordJohnsonAlgo() took " << lstElapsed.count()
+			<< " seconds to sort a list! :)\n";
 	}
 	catch (std::exception& e) {
 		std::cerr << e.what() << "\n";
