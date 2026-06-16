6473b72fcecef25cdbe73918c2b0284595f485c9
Thomas Black-Roff <thblack-@c1r6p13.hive.fi>
2026-06-16 15:58:39 +0300

    bug: cpp09/ex01 found incorrect implementation of RPN, rebuild needed

--- cpp09/ex01/src/RPN.cpp
+++ cpp09/ex01/src/RPN.cpp
@@ -15,85 +15,91 @@
 #include <stdexcept> // for std::runtime_error
 #include <limits.h> // for INT_MAX and INT_MIN
 
-std::deque<int>	RPN::parseInput( const char *in ) {
-	std::string	str(in);
-	// Early throws
-	if (str.length() == 0)
-		throw std::runtime_error("no input");
-	if (str.find_first_not_of(numChars) != std::string::npos)
-		throw std::runtime_error("invalid char");
-	if (!std::strchr(digitChars, str[str.find_first_not_of(' ')]))
-		throw std::runtime_error("invalid order");
-
-	// Fill stack
-	std::deque<int>	result;
-	bool	numToggle = true;
-	bool	spaceToggle = true;
-	auto it = str.begin();
-	while (*it == ' ')
-		++it;
-	result.push_back(*it++);
-	while (it != str.end()) {
-		if (*it == ' ') {
-			spaceToggle = false;
-			++it;
-			continue;
-		}
-		// Throw invalid order
-		if (spaceToggle == true && (std::strchr(digitChars, *it) || std::strchr(opChars, *it)))
-			throw std::runtime_error("unexpected instruction, only single-char instructions allowed");
-		if (numToggle == true && !std::strchr(digitChars, *it))
-			throw std::runtime_error("unexpected operator");
-		else if (numToggle == false && !std::strchr(opChars, *it))
-			throw std::runtime_error("unexpected digit");
-		result.push_back(*it);
-		numToggle = !numToggle;
-		spaceToggle = true;
-		++it;
-	}
-	if (result.size() < 3)
-		throw std::runtime_error("not enough instructions to calculate");
-	if (result.size() % 2 != 1)
-		throw std::runtime_error("invalid syntax");
-	return result;
-};
-
-int	RPN::calculate( std::deque<int>& equation ) {
-	long	left;
-	int		right;
-	char	operation;
-	left = equation.front() - atoiASCII;
-	equation.pop_front();
-	while (equation.size() > 1) {
-		right = equation.front() - atoiASCII;
-		equation.pop_front();
-		operation = equation.front();
-		equation.pop_front();
-		switch (operation) {
-			case '+': {
-				left += right;
-				break;
-			}
-			case '-': {
-				left -= right;
-				break;
-			}
-			case '*': {
-				left *= right;
-				break;
-			}
-			case '/': {
-				if (right == 0)
-					throw std::runtime_error("division by zero");
-				left /= right;
-			}
-		}
-		if (left > INT_MAX)
-			throw std::runtime_error("integer too big");
-		if (left < INT_MIN)
-			throw std::runtime_error("integer too small");
-	}
-	if (equation.size() > 0)
-		throw std::runtime_error("invalid syntax at calculation stage");
-	return static_cast<int>(left);
-};
+// std::deque<int>	RPN::parseInput( const char *in ) {
+// 	std::string	str(in);
+// 	// Early throws
+// 	if (str.length() == 0)
+// 		throw std::runtime_error("no input");
+// 	if (str.find_first_not_of(numChars) != std::string::npos)
+// 		throw std::runtime_error("invalid char");
+// 	if (!std::strchr(digitChars, str[str.find_first_not_of(' ')]))
+// 		throw std::runtime_error("invalid order");
+//
+// 	// Fill stack
+// 	std::deque<int>	result;
+// 	size_t	precedenceLevel = 0;
+// 	bool	spaceToggle = true;
+// 	auto it = str.begin();
+// 	while (*it == ' ')
+// 		++it;
+// 	result.push_back(*it++);
+// 	while (it != str.end()) {
+// 		if (*it == ' ') {
+// 			spaceToggle = false;
+// 			++it;
+// 			continue;
+// 		}
+// 		// Throw invalid order
+// 		if (spaceToggle == true && (std::strchr(digitChars, *it) || std::strchr(opChars, *it)))
+// 			throw std::runtime_error("unexpected instruction, only single-char instructions allowed");
+// 		if (precedenceLevel == 0 && !std::strchr(digitChars, *it))
+// 			throw std::runtime_error("unexpected operator");
+// 		// else if (numToggle == false && !std::strchr(opChars, *it))
+// 		// 	throw std::runtime_error("unexpected digit");
+// 		if (std::strchr(digitChars, *it))
+// 			++precedenceLevel;
+// 		else if (std::strchr(opChars, *it))
+// 			--precedenceLevel;
+// 		result.push_back(*it);
+// 		// numToggle = !numToggle;
+// 		spaceToggle = true;
+// 		++it;
+// 	}
+// 	if (result.size() < 3)
+// 		throw std::runtime_error("not enough instructions to calculate");
+// 	else if (result.size() % 2 != 1)
+// 		throw std::runtime_error("invalid syntax");
+// 	else if (precedenceLevel != 0)
+// 		throw std::runtime_error("not enough operators to calculate");
+// 	return result;
+// };
+//
+// int	RPN::calculate( std::deque<int>& equation ) {
+// 	long	left;
+// 	int		right;
+// 	char	operation;
+// 	left = equation.front() - atoiASCII;
+// 	equation.pop_front();
+// 	while (equation.size() > 1) {
+// 		right = equation.front() - atoiASCII;
+// 		equation.pop_front();
+// 		operation = equation.front();
+// 		equation.pop_front();
+// 		switch (operation) {
+// 			case '+': {
+// 				left += right;
+// 				break;
+// 			}
+// 			case '-': {
+// 				left -= right;
+// 				break;
+// 			}
+// 			case '*': {
+// 				left *= right;
+// 				break;
+// 			}
+// 			case '/': {
+// 				if (right == 0)
+// 					throw std::runtime_error("division by zero");
+// 				left /= right;
+// 			}
+// 		}
+// 		if (left > INT_MAX)
+// 			throw std::runtime_error("integer too big");
+// 		if (left < INT_MIN)
+// 			throw std::runtime_error("integer too small");
+// 	}
+// 	if (equation.size() > 0)
+// 		throw std::runtime_error("invalid syntax at calculation stage");
+// 	return static_cast<int>(left);
+// };
--- cpp09/ex02/src/main.cpp
+++ cpp09/ex02/src/main.cpp
@@ -25,35 +25,26 @@ int	main(int argv, char **argc) {
 	list	lstInput, lstOutput;
 	try {
 		vecInput = PmergeMe<vector>::loadData(argc + 1);
-		std::cout << "VECTOR\n";
-		std::cout << "Before: " << vecInput << "\n";
 		auto	vecStart = std::chrono::steady_clock::now();
 		vecOutput = PmergeMe<vector>::fordJohnsonAlgo(vecInput);
 		auto	vecEnd = std::chrono::steady_clock::now();
-		std::cout << "After: " << vecOutput << "\n";
-		std::cout << "Vector is" << (PmergeMe<vector>::isSort(vecOutput) ? " " : " not ") << "sorted!\n";
-
 
 		lstInput = PmergeMe<list>::loadData(argc + 1);
-		std::cout << "LIST\n";
-		std::cout << "Before: " << lstInput << "\n";
 		auto	lstStart = std::chrono::steady_clock::now();
 		lstOutput = PmergeMe<list>::fordJohnsonAlgo(lstInput);
 		auto	lstEnd = std::chrono::steady_clock::now();
-		std::cout << "After: " << lstOutput << "\n";
-		std::cout << "List is" << (PmergeMe<list>::isSort(lstOutput) ? " " : " not ") << "sorted!\n";
-
+		std::cout << "Before: " << vecInput << "\n";
+		std::cout << "After: " << vecOutput << "\n";
 
 		std::chrono::duration<double>	vecElapsed = vecEnd - vecStart;
 		std::chrono::duration<double>	lstElapsed = lstEnd - lstStart;
-		std::cout << "RESULTS\n";
-		std::cout << "fordJohnsonAlgo() took " << vecElapsed.count()
-			<< " seconds to sort a vector! :)\n";
-		std::cout << "fordJohnsonAlgo() took " << lstElapsed.count()
-			<< " seconds to sort a list! :)\n";
+		std::cout << "Time to process a range of " << vecInput.size()
+		<< " elements with std::vector : " << vecElapsed.count() << " seconds\n";
+		std::cout << "Time to process a range of " << lstInput.size()
+		<< " elements with std::list : " << lstElapsed.count() << " seconds \n";
 	}
 	catch (std::exception& e) {
-		std::cerr << e.what() << "\n";
+		std::cerr << "Error: " << e.what() << "\n";
 	}
 	return EXIT_SUCCESS;
 };
